27133e19d35ed7947fa09c81eeed065be9de1924
Shekoni Ayomide Shakiru <shekonishakiru@Shekonis-MacBook-Pro.local>
2023-07-10 10:51:00 +0100

    vercel-error-fix-v6

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,11 +21,5 @@ module.exports = {
     },
   },
   plugins: [],
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./Layout/**/*.{js,ts,jsx,tsx,mdx}",
-
-  ],
 }
 
