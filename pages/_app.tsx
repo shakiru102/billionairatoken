92c4384f301230db6131ada6e2aa1d74f934096d
Shekoni Ayomide Shakiru <shekonishakiru@Shekonis-MacBook-Pro.local>
2023-07-10 10:57:49 +0100

    vercel-error-fix-v7

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
+import { StyledEngineProvider } from '@mui/material'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div className='bg-black min-h-screen'>
-          <Component {...pageProps} />
-        </div>
+  return <StyledEngineProvider injectFirst>
+              <div className='bg-black min-h-screen'>
+                <Component {...pageProps} />
+              </div>
+        </StyledEngineProvider>
+  
 }
 
 export default MyApp
